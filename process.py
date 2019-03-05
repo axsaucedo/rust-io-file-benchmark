5c3378d9c56d6bd520d037ade282c924d9398c8a
Alejandro Saucedo <axsauze@gmail.com>
2019-03-05 16:33:46 +0000

    removed full load to memory

--- process.py
+++ process.py
@@ -4,21 +4,24 @@ import os, sys
 in_path = sys.argv[1]
 out_path = sys.argv[2]
 
-in_f = open(in_path)
-
-with open(out_path, "w") as out_f: 
-    idx = 0
-    row = []
-    for l in in_f.readlines()[1:]:
-        if idx < 3:
-            row.append(l.replace("\n", "")[2:])
-            row.append('\t')
-            idx = idx + 1
-        else:
-            row.append("\n")
-            out_f.write("".join(row))
-            row = []
-            idx = 0
+with open(in_path) as in_f:
+    with open(out_path, "w") as out_f:
+        idx = 0
+        row = []
+        # skip first line
+        in_f.readline()
+        l = in_f.readline()
+        while l:
+            if idx < 3:
+                row.append(l.replace("\n", "")[2:])
+                row.append('\t')
+                idx = idx + 1
+            else:
+                row.append("\n")
+                out_f.write("".join(row))
+                row = []
+                idx = 0
+            l = in_f.readline()
 
 
 
